79259a3de38f540184f1089ac2a2b86307e374a2
ViniBelo <vinialvesbelo@outlook.com>
2025-12-27 16:06:01 -0300

    Add connection to WebSocket server and sending message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,58 @@
+"use client";
+
+import { Client } from "@stomp/stompjs";
+import { useEffect, useRef, useState } from "react";
+
 export default function Home() {
+  const topic = "/topic/chat";
+  const [message, setMessage] = useState("");
+
+  const clientRef = useRef<Client>(null);
+
+  useEffect(() => {
+    const client = new Client({
+      brokerURL: "ws://localhost:8080/ws",
+      onConnect: () => {
+        client.subscribe(topic, (message) =>
+          console.log(`Received: ${message.body}`),
+        );
+        client.publish({ destination: topic, body: "Hello, World!" });
+      },
+      onDisconnect: () => {
+        console.log("Disconnected");
+      },
+    });
+    client.activate();
+    clientRef.current = client;
+
+    return () => {
+      client.deactivate();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (clientRef.current && clientRef.current.connected) {
+      clientRef.current.publish({ destination: topic, body: message });
+      setMessage("");
+    } else {
+      console.log("Not connected to the WebSocket server");
+    }
+  };
+
   return (
-    <div className="flex justify-center items-center min-w-screen min-h-screen">
-      <h1>Hello, world!</h1>
+    <div className="flex flex-col justify-center items-center min-w-screen min-h-screen gap-3">
+      <input
+        className="bg-gray-700 border-s-violet-100 p-3 rounded-3xl size-12 w-3xs"
+        type="text"
+        value={message}
+        onChange={(newMessage) => setMessage(newMessage.target.value)}
+      ></input>
+      <button
+        className="bg-gray-700 border-s-violet-100 p-3 rounded-3xl size-12 w-3xs"
+        onClick={sendMessage}
+      >
+        Send message
+      </button>
     </div>
   );
 }
